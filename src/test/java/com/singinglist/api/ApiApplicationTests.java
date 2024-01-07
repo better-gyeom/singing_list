151f2ff6e7b3113f621c22c0cb8e6c32a5823023
better-gyeom <better.gyeom@gmail.com>
2024-01-07 23:32:42 +0900

    init

---
+++ src/test/java/com/singinglist/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singinglist.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
